41f94452ad5151c930d775a42d113ea986da22b3
gary <gary.white@lodgit.com>
2021-07-07 16:24:01 +0200

    Arrggghhhh

--- audio.js
+++ audio.js
@@ -16,16 +16,18 @@ var Audio = {
         sound.buffer = this.bufferList[i];
         sound.start(0);
         sound.stop(this.audioContext.currentTime + 18); // remove .currentTime + 18 and sound doesn't repeat
-    },    
+    },
+   
 };
 
         
-    function stopFunction() {
-        console.log("Stop button pressed");
-        // document.getElementById('canvas').width = 500;
-        // var sound = Audio.audioContext.createBufferSource();
+    // function stopFunction() {
+    //     console.log("Stop button pressed");
+        
+    //     // document.getElementById('canvas').width = 500;
+    //     // var sound = Audio.audioContext.createBufferSource();
 
-    }
+    // }
 
 
 
